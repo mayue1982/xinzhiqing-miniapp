d3ec08ed4990d78e7a970519ba088e27ed1527b6
mayue1982 <129023750+mayue1982@users.noreply.github.com>
2026-07-05 13:27:10 +0800

    Add home carousel visual assets

--- pages/home/index.js
+++ pages/home/index.js
@@ -2,6 +2,23 @@ const api = require('../../services/api')
 
 Page({
   data: {
+    carouselItems: [
+      {
+        image: '/assets/images/carousel-stars.jpg',
+        title: '星空成长旅程',
+        subtitle: 'MIND JOURNEY'
+      },
+      {
+        image: '/assets/images/carousel-healing.jpg',
+        title: '自然疗愈恢复',
+        subtitle: 'NATURE HEALING'
+      },
+      {
+        image: '/assets/images/carousel-culture.jpg',
+        title: '文化游学体验',
+        subtitle: 'CULTURAL STUDY'
+      }
+    ],
     quickItems: [],
     levels: [],
     routes: []
@@ -13,10 +30,19 @@ Page({
   },
   async loadPage() {
     const res = await api.getBootstrap()
+    const routeImages = [
+      '/assets/images/carousel-stars.jpg',
+      '/assets/images/carousel-healing.jpg',
+      '/assets/images/carousel-culture.jpg',
+      '/assets/images/carousel-stars.jpg'
+    ]
     this.setData({
       quickItems: res.data.quickItems || [],
       levels: res.data.levels || [],
-      routes: res.data.routes || []
+      routes: (res.data.routes || []).map((item, index) => ({
+        ...item,
+        image: routeImages[index % routeImages.length]
+      }))
     })
   },
   openRoute(e) {
